068ac2864b8192245c9db548659b5a9caacb05b5
Marc Schmidt <code@masch212.de>
2025-04-05 15:26:55 +0200

    chore(prettier): fix shareable config for some projects

---
+++ prettier/index.js
@@ -0,0 +1,20 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  useTabs: false,
+  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+};
+
+export default config;
